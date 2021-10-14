a56c00f72fbfd197026e994ef0c8ea9578a003ff
Brian <brian.hardy67@gmail.com>
2021-10-14 15:31:49 -0400

    functions all work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch, useParams } from "react-router-dom";
 
-function App() {
+
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+            <li>
+              <Link to="/Cart">View Cart</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/Products">
+            <Products />
+          </Route>
+          <Route path="/Cart">
+            <Cart />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+//list of all products
+function Products() {
+  let match = useRouteMatch();
+
+  return (
+    <div>
+      <h2>Products for Engineers</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/Rubber-Ducky`}>Rubber Ducky</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/Tylenol`}>
+            Tylenol 
+          </Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/A-Good-Chair`}>
+            A good chair 
+          </Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path={`${match.path}/:productId`}>
+          <Product />
+        </Route>
+      </Switch>
     </div>
   );
 }
 
-export default App;
+//here are your products
+function Product(item) {
+  let { productId } = useParams();
+
+  return (
+    <div>
+      <h3>Hey you chose {productId}!</h3>
+      <p>I'll work on getting that in your cart</p>
+      <p>You'll need {productId} ASAP</p>
+    </div>
+  )
+}
+
+
+function Cart() {
+  return (
+    <div>
+      <h2>Cart</h2>
+      <button>If you are ready to leave click me!</button>
+    </div>
+  )
+}
